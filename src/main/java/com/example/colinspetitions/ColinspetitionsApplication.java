7227519b7a96801a48c22b3c19d32ab151f992ab
colinr1 <colinregan123@gmail.com>
2024-11-15 09:58:57 +0000

    Initial commit

---
+++ src/main/java/com/example/colinspetitions/ColinspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.colinspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColinspetitionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColinspetitionsApplication.class, args);
+    }
+
+}
